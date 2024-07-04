482f3c456e7299b02c285345800879810d594a28
William Mburu <williammburu79@gmail.com>
2024-07-04 05:44:44 +0300

    new file:   client/.eslintrc.cjs 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/index.html 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/postcss.config.js 	new file:   client/public/vite.svg 	new file:   client/src/App.css 	new file:   client/src/App.jsx 	new file:   client/src/assets/react.svg 	new file:   client/src/index.css 	new file:   client/src/main.jsx 	new file:   client/tailwind.config.js 	new file:   client/vite.config.js 	new file:   server/.gitignore 	new file:   server/index.js 	new file:   server/package-lock.json 	new file:   server/package.json

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express"; // handle routes, middleware, templates...
+import cors from "cors"; // middleware for handling cross-origin requests
+
+
+app.listen(5000, () => {
+    console.log('server running on port 5000');
+});
